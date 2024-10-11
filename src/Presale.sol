998c52b3088d49c41961f674da48a614af8993f2
jecombe <jeremcombe@hotmail.fr>
2024-10-11 17:54:47 +0200

    update: commit for dep

--- src/Presale.sol
+++ src/Presale.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
-
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { IUniswapV2Router02 } from "v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol';
 
 contract Presale {
     IERC20 public token;
